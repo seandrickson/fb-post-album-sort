9a78f5c88434c4e7e1a4ae9b9515f2669ba4aa95
Sean Seanandrosie Rosie <seanandrosie@seans-mbp-2.lan>
2022-10-18 10:51:02 -0400

    Update methods for more async

--- app.mjs
+++ app.mjs
@@ -1,39 +1,43 @@
-import fs from 'fs/promises';
-import path from 'path';
+import {
+  copyFile,
+  mkdir,
+  readFile
+} from 'node:fs/promises';
+import {
+  parse,
+  resolve
+} from 'node:path';
 
 const SRC_FOLDER = './fb-export';
 const DIST_FOLDER = './dist';
-const JSON_FILE = SRC_FOLDER + '/posts/posts_1.json';
 
 const fb_posts = JSON.parse(
-  await fs.readFile(new URL(JSON_FILE, import.meta.url))
+  await readFile(
+    new URL(`${SRC_FOLDER}/posts/posts_1.json`, import.meta.url)
+  )
 );
 
 const create_dir = async (post) => {
   const title = post.replace(/\n/g, ' | ')
     .replace(/\s+/g, ' ')
     .replace(/\W+/g, '-');
-  const new_path = path.resolve(DIST_FOLDER, title);
-  await fs.mkdir(new_path, { recursive: true });
+  const new_path = resolve(DIST_FOLDER, title);
+  await mkdir(new_path, { recursive: true });
   return new_path;
-}
-
-const copy_photos = (photos, new_path) => {
-  return Promise.all(photos.map(photo => {
-    const orig = path.resolve(SRC_FOLDER, photo.media.uri);
-    const { base } = path.parse(orig);
-    console.log([
-      'COPY FILE:',
-      `FROM: ${path.relative(process.cwd(), orig)}`,
-      `TO: ${path.relative(process.cwd(), path.resolve(new_path, base))}`,
-    ].join('\n'), '\n');
-    return fs.copyFile(orig, path.resolve(new_path, base));
-  }));
 };
 
+const copy_photos = async (photos, new_path) => await photos.map((photo) => {
+  const orig = resolve(SRC_FOLDER, photo.media.uri);
+  const { base } = parse(orig);
+  return copyFile(orig, resolve(new_path, base));
+});
+
 const process_posts = async (post) => {
-  const new_path = await create_dir(post.data[0].post);
-  await copy_photos(post.attachments[0].data, new_path);
+  const post_title = post.data[0]?.post;
+  const post_photos = post.attachments[0]?.data;
+  if (!post_title) return;
+  const new_path = await create_dir(post_title);
+  await copy_photos(post_photos, new_path);
 };
 
 if (Array.isArray(fb_posts)) {
