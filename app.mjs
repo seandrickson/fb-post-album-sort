eddac2d294f717c3c8ee5533fdc6db69d2c451bc
Sean Hendrickson <1627776+seandrickson@users.noreply.github.com>
2023-09-07 10:52:36 -0400

    Cleanup code, improve output

--- app.mjs
+++ app.mjs
@@ -1,36 +1,33 @@
-import {
-  copyFile,
-  mkdir
-} from 'node:fs/promises';
-import {
-  parse,
-  resolve
-} from 'node:path';
+import { copyFile, mkdir } from "node:fs/promises";
+import { basename, resolve } from "node:path";
 import fb_posts from "./fb-export/posts/profile_posts_1.json" assert { type: "json" };
-const SRC_FOLDER = './fb-export';
-const DIST_FOLDER = './dist';
+const SRC_FOLDER = "./fb-export";
+const DIST_FOLDER = "./dist";
 
 const create_dir = async (post) => {
-  const title = post.replace(/\n/g, ' | ')
-    .replace(/\s+/g, ' ')
-    .replace(/\W+/g, '-');
+  const title = post
+    .trim()
+    .replace(/\n/g, " | ")
+    .replace(/\s+/g, " ")
+    .replace(/[^A-Za-z0-9\s-|]+/g, "");
   const new_path = resolve(DIST_FOLDER, title);
   await mkdir(new_path, { recursive: true });
   return new_path;
 };
 
-const copy_photos = async (photos, new_path) => await photos.map((photo) => {
-  const orig = resolve(SRC_FOLDER, photo.media.uri);
-  const { base } = parse(orig);
-  return copyFile(orig, resolve(new_path, base));
-});
+const copy_photos = (photos, new_path) =>
+  photos.map((photo) =>
+    copyFile(
+      resolve(SRC_FOLDER, photo.media.uri),
+      resolve(new_path, basename(photo.media.uri))
+    )
+  );
 
 const process_posts = async (post) => {
-  const post_title = post.data[0]?.post;
+  const post_title = post.data[0]?.post ?? "Untitled";
   const post_photos = post.attachments[0]?.data;
-  if (!post_title) return;
   const new_path = await create_dir(post_title);
-  await copy_photos(post_photos, new_path);
+  await Promise.all(copy_photos(post_photos, new_path));
 };
 
 if (Array.isArray(fb_posts)) {
